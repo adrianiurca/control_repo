e13a9ad2078aba66bff03873f44f20bd0d17f0f8
Adrian Iurca <45214757+adrianiurca@users.noreply.github.com>
2021-01-05 02:31:13 +0200

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,9 @@
 class profile::agent_nodes {
   include dockeragent
-  dockeragent::node {'web.puppet.vm': }
-  dockeragent::node {'db.puppet.vm': }
+  dockeragent::node {'web.puppet.vm':
+    include profile::ssh_server
+  }
+  dockeragent::node {'db.puppet.vm':
+    include profile::ssh_server
+  }
 }
